961dc03a6425546f7963fb13371cb617b565b222
georges nodari <nodari-g@hotmail.com>
2022-03-31 19:14:19 +0200

    projet finaliser

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Error from './components/Error'
-import Freelances from './pages/Freelances'
-import Results from './components/Results'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
-
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Error from './components/Error'
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/survey/:questionNumber" element={<Survey />} />
-        <Route exact path="/results" element={<Results />} />
-        <Route exact path="/freelances" element={<Freelances />} />
-        <Route exact path='*' element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/survey/:questionNumber" element={<Survey />} />
+            <Route exact path="/results" element={<Results />} />
+            <Route exact path="/freelances" element={<Freelances />} />
+            <Route exact path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
-
